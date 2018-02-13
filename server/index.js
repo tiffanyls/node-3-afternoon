b7f04fa1ec79bf308d11a7820afd028bb15e7f7e
Tiffany Schureman <Tiffany@Tiffanys-MacBook-Air.local>
2018-02-13 15:33:28 -0600

    step 8 and 9 done

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const session = require('express-session');
 const checkForSession = require('./middlewares/checkForSession');
 const swag_controller = require('./controllers/swag_controllers');
 const auth_controller = require('./controllers/auth_controller');
+const cart_controller = require('./controllers/cart_controller');
 require ('dotenv').config();
 
 
@@ -23,6 +24,9 @@ app.post('/api/login', auth_controller.login);
 app.post('/api/register', auth_controller.register);
 app.post('/api/signout', auth_controller.signout);
 app.get('/api/user', auth_controller.getUser);
+app.post('/api/cart', cart_controller.add);
+app.post('/api/cart/checkout', cart_controller.checkout);
+app.delete('/api/cart', cart_controller.delete);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {console.log(`Server listening on port ${port}.`)});
